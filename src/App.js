ed3384a8be4ed24348c0cb851c3aa77b7e1c3f69
Robwert Mota <robwert7991@gmail.com>
2019-01-19 00:16:53 -0400

    dependencies and some components added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { Button,Form, FormGroup, Input, FormText,Label } from 'reactstrap';
 import './App.css';
 
-class App extends Component {
+class TaskBar extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      input: '',
+      date: new Date().toLocaleTimeString()
+    }
+     
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+
+  }
+
+  handleChange(e){
+    this.setState({input: e.target.value});
+  }
+  render(){
+    return(
+      <Form onSubmit={this.handleSubmit}>
+        <FormGroup>
+          <Label for="taskText">Task : </Label>
+          <Input type="text" id="taskText" placeholder={"agregar a las "+this.state.date} value={this.state.input} onChange={this.handleChange}/>
+          <Button color="danger">Agregar</Button>
+        </FormGroup>
+      </Form>
+    );
+  }
+}
+
+function Welcome(props){
+  return <h4>Hello, {props.name}.</h4>
+}
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrapper">
+        <Welcome name="Anthony" />
+        <TaskBar />
+
       </div>
     );
   }
